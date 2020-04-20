fd40d4054c52d66d8128e7601ac0f4f665455ddb
Jagan R <37062981+jaganchoccy@users.noreply.github.com>
2020-04-20 22:58:47 +0530

    care

---
+++ src/app/app.module.ts
@@ -0,0 +1,70 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+// FontAwesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ToastrModule } from 'ngx-toastr'; //alert
+//Http
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from '../app/CommonServices/interceptor.service';
+
+//ngx
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+//primeng
+import { ButtonModule } from 'primeng/button';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+//Components
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    PageNotFoundComponent,
+    ToolbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    BsDatepickerModule.forRoot(),
+    AppRoutingModule,
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 6000,
+      positionClass: 'toast-top-center',
+      preventDuplicates: true,
+      enableHtml: true,
+    }),
+    ReactiveFormsModule,
+    HttpClientModule,
+    ProgressSpinnerModule,
+    ButtonModule,
+    AutoCompleteModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
